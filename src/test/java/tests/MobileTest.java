ff8ab1e05dc6a0d0c82a2769e539e99b1be64470
schwester34 <yana64604@gmail.com>
2022-07-01 20:12:23 +0500

    add smth

--- src/test/java/tests/MobileTest.java
+++ src/test/java/tests/MobileTest.java
@@ -2,23 +2,28 @@ package tests;
 import com.codeborne.selenide.CollectionCondition;
 import io.appium.java_client.AppiumBy;
 import org.junit.jupiter.api.Test;
+
+
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
-
-public class MobileTest extends TestBase {
-
-    @Test
-    void searchTest() {
-        back();
-        step("Type search", () -> {
-            $(AppiumBy.id("org.wikipedia.alpha:id/search_container")).click();
-            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("tchaikovsky");
-        });
-        step("Verify content found", () ->
-                $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title"))
-                        .shouldHave(CollectionCondition.containExactTextsCaseSensitive("Pyotr Ilyich Tchaikovsky"))
-        );
+    public class MobileTest extends TestBase {
+        @Test
+        void searchTest() {
+            back();
+            step("Type search", () -> {
+                $(AppiumBy.accessibilityId("Search Wikipedia")).click();
+                $(AppiumBy.accessibilityId("Search text")).sendKeys("tchaikovsky");
+                //$(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("tchaikovsky");
+            });
+            step("Verify content found", () ->
+                    $$(AppiumBy.accessibilityId("Search page list item title"))
+                    //$$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title"))
+                            .shouldHave(CollectionCondition.containExactTextsCaseSensitive("Pyotr Ilyich Tchaikovsky"))
+            );
+        }
     }
-
-}
+//org.wikipedia.alpha:id/search_src_text
+//org.wikipedia.alpha:id/page_list_item_title
+//AppiumBy.id("org.wikipedia.alpha:id/search_container")).click();
+//findElement(By.className("Search Wikipedia"));
